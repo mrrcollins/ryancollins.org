40379d758d8a02b80ea6011b30acfd2ea9353147
Ryan Collins <github@ryancollins.org>
2013-05-23 14:45:38 -0400

    Almost finished with timed posting script

--- dbupdate.sh
+++ dbupdate.sh
@@ -1,10 +1,22 @@
-#!/bin/sh
+#!/bin/bash
+
+shopt -s nullglob
 
 YEAR=`date +%Y`
+TOPOST=/home/gozar/Dropbox/Elements/RyanCollins.org/ToPost/*.markdown
+NOW=`date +"%Y-%m-%d %H:%M"`
+for file in ${TOPOST}
+do
+    echo "Checking ${file}..."
+    DATELINE=`cat "${file}" | grep "^Date: "`
+    if [[ ${DATELINE#* } < ${NOW} ]]; then
+        echo "Time to post"
+    fi
+done
 
-cd ~/Dropbox/Elements/RyanCollins.org/ToPost
-mv * ~/Development/ryancollins.org/content/${YEAR}/
 
-cd ~/Development/ryancollins.org
-make html
+#mv * ~/Development/ryancollins.org/content/${YEAR}/
+
+#cd ~/Development/ryancollins.org
+#make html
 
