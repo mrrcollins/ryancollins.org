9f9f8e46e8dc5d2c6ba3ddb1d5d518d7fac9f1d8
Ryan Collins <github@ryancollins.org>
2013-05-26 08:25:23 -0400

    Finish update from DropBox script

--- dbupdate.sh
+++ dbupdate.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
 
+TOPOST=~/Dropbox/Elements/RyanCollins.org/ToPost/*.markdown
+SITE=~/Development/repos/ryancollins.org
+
 shopt -s nullglob
 
 YEAR=`date +%Y`
-TOPOST=/home/gozar/Dropbox/Elements/RyanCollins.org/ToPost/*.markdown
 NOW=`date +"%Y-%m-%d %H:%M"`
+
 for file in ${TOPOST}
 do
-    echo "Checking ${file}..."
     DATELINE=`cat "${file}" | grep "^Date: "`
     if [[ ${DATELINE#* } < ${NOW} ]]; then
-        echo "Time to post"
+        cp "${file}" "${SITE}/content/${YEAR}"
+        cd "${SITE}"
+        make html
     fi
 done
-
-
-#mv * ~/Development/ryancollins.org/content/${YEAR}/
-
-#cd ~/Development/ryancollins.org
-#make html
 
